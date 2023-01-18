3e0a95fa835821af9bcd1183786e4e62cae86a8e
Yura123990 <yurgame2009s@gmail.com>
2023-01-18 12:40:16 +0200

    Create match-answers.php

---
+++ match-answers.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>З'єднай відповіді</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;500&family=Roboto+Mono:wght@100;300;400;500;600;700&display=swap" rel="stylesheet">
+    <style>
+        label{
+            font-size:100%;
+            font-family: 'Montserrat', sans-serif;
+            color: khaki;
+        }
+    </style>
+</head>
+<body>
+    <h1 class="main-heading">З'єднайте факти про Святих Миколаїв у різному світі з їхніми іменами</h1>
+
+    <form action="#" method="post">
+        <!--  -->
+        <h3></h3>
+        <select name="sl1">
+            <option value="">Баббо Натале</option>
+            <option value="">Святий Миколай</option>
+            <option value="">Санта Клаус</option>
+            <option value="">Йоулупуккі</option>
+        </select>
+        <!--  -->
+    </form>
+
+    <footer>
+            <h3>Yura Seniura</h3>
+            <p class="for-footer">©2023 New Year wishing generator. All rights reserved.</p>
+    </footer>
+</body>
+</html>
